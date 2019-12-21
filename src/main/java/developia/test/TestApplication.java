2958b4f9b7c5a6eebfaaf44f8b4e9d5d6104e2b7
Shahriyar Novruzov <shahriyar.novruzov@pashabank.az>
2019-12-21 18:17:42 +0400

    first commit

---
+++ src/main/java/developia/test/TestApplication.java
@@ -0,0 +1,13 @@
+package developia.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+
+}
